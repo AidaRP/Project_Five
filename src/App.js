86b6cef0ad523eb250d2fdbc6cddd5356c454b93
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-07 19:09:32 +0100

    add Header, Footer, Home, Login and Register imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './containers/Home/Home';
+import Login from './containers/Login/Login';
+import Register from './containers/Register/Register';
 
 
 function App() {
