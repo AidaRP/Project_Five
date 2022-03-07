c1b8ce55e58773c4fe8476a18b87939293cab98c
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-07 19:23:35 +0100

    fill in Home.jsx of components folder

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+import './Home.scss'
+
+const Home = () => {
+  
+    return (
+    <div className='colorsHome'>
+        Hi Darlings im home
+    </div>
+  )
+}
+
+export default Home;
