f757aa8abbf1998ded160610c54fa6b91a1d3777
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-07 18:57:16 +0100

    add Header && Footer in BrowserRouter. Home, Login, Register in Route of Routes.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,22 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      
+      <BrowserRouter>
+      
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+
+      </Routes>
+
+      <Footer/>
     
+    </BrowserRouter>
+
     </div>
   );
 }
