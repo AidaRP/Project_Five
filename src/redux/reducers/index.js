d6caac500d977691fba9117028fa553161af2cf9
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-09 18:59:38 +0100

    add a files store.js, types.js in the redux folder. Create a reducers folder with index.js file in the redux folder.

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+// import {combineReducers} from 'redux';
+
+// import credentials from './datosLogin-reducer';
+// import search from './busquedaFilms-reducer';
+
+// const rootReducer = combineReducers({
+//     credentials,
+//     search
+// });
+
+// export default rootReducer;
